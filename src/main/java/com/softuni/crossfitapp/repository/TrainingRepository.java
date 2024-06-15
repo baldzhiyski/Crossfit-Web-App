1598cf31c3b076c08d1c19364ba9daaa3cc66342
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2024-06-15 23:54:26 +0300

    Adding the head and the footer to the index page dynamically and spring dev-tools

--- src/main/java/com/softuni/crossfitapp/repository/TrainingRepository.java
+++ src/main/java/com/softuni/crossfitapp/repository/TrainingRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface Training extends JpaRepository<Training, UUID> {
+public interface TrainingRepository extends JpaRepository<com.softuni.crossfitapp.model.entity.Training, UUID> {
 }
