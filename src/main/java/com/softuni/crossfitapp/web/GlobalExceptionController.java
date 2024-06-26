c1a77be0702de01b88058126a99104992a74058a
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2024-06-26 23:38:32 +0300

    Other improvements of the pages

--- src/main/java/com/softuni/crossfitapp/web/GlobalExceptionController.java
+++ src/main/java/com/softuni/crossfitapp/web/GlobalExceptionController.java
@@ -7,14 +7,13 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionController {
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(ObjectNotFoundException.class)
-    public ModelAndView handleNotFound(ObjectNotFoundException exception) {
+    @ExceptionHandler({ObjectNotFoundException.class, NoResourceFoundException.class})
+    public ModelAndView handleNotFound(Exception exception) {
         ModelAndView modelAndView = new ModelAndView("not-found");
-        modelAndView.addObject("id", exception.getId());
         return modelAndView;
     }
 }
